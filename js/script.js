d830381b52cf5ae83b65996017031ff9c4fed1d8
mahdi cheikhrouhou <61123610+Mtaylorr@users.noreply.github.com>
2021-09-09 02:14:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 	});
 
 	var typed  = new Typed(".typed", {
-		strings : ["Student.", "Competitive programmer."], 
+		strings : ["Engineering student.", "Competitive programmer."], 
 		typeSpeed:70,
 		loop:true,
 		startDelay : 1000,
